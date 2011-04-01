7e514f7e89a52637995ccfb25c86f84b1fbce7b7
Daniel Bruce <ircubic@gmail.com>
2011-04-01 15:54:16 +0200

    Added Android Prototype skeleton

---
+++ prototype/src/net/ircubic/eventmap/EventMap.java
@@ -0,0 +1,14 @@
+package net.ircubic.eventmap;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class EventMap extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+    }
+}
