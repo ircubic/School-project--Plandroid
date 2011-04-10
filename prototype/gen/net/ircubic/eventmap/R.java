3421f8f971691d25d2a42ee700926dc63c3d9f9a
Daniel Bruce <ircubic@gmail.com>
2011-04-11 00:06:41 +0200

    Added time choosing logic

--- prototype/gen/net/ircubic/eventmap/R.java
+++ prototype/gen/net/ircubic/eventmap/R.java
@@ -14,18 +14,22 @@ public final class R {
         public static final int icon=0x7f020000;
     }
     public static final class id {
-        public static final int editEndTime=0x7f050005;
-        public static final int editStartTime=0x7f050003;
-        public static final int radio0=0x7f050007;
-        public static final int radio1=0x7f050008;
-        public static final int radio2=0x7f050009;
-        public static final int radioButton1=0x7f05000a;
-        public static final int radioGroup1=0x7f050006;
-        public static final int scenarioStartButton=0x7f05000b;
+        public static final int editEndDate=0x7f050008;
+        public static final int editEndTime=0x7f050009;
+        public static final int editStartDate=0x7f050004;
+        public static final int editStartTime=0x7f050005;
+        public static final int linearLayout1=0x7f050003;
+        public static final int linearLayout2=0x7f050007;
+        public static final int radio0=0x7f05000b;
+        public static final int radio1=0x7f05000c;
+        public static final int radio2=0x7f05000d;
+        public static final int radioButton1=0x7f05000e;
+        public static final int radioGroup1=0x7f05000a;
+        public static final int scenarioStartButton=0x7f05000f;
         public static final int textEventTitle=0x7f050001;
         public static final int textView1=0x7f050000;
         public static final int textView2=0x7f050002;
-        public static final int textView3=0x7f050004;
+        public static final int textView3=0x7f050006;
     }
     public static final class layout {
         public static final int event_creation=0x7f030000;
--- prototype/src/net/ircubic/eventmap/EventCreation.java
+++ prototype/src/net/ircubic/eventmap/EventCreation.java
@@ -1,49 +1,74 @@
 package net.ircubic.eventmap;
 
 import java.util.Calendar;
-import java.util.Date;
 import java.util.GregorianCalendar;
 
 import android.app.Activity;
-import android.location.Location;
+import android.app.DatePickerDialog;
+import android.app.DatePickerDialog.OnDateSetListener;
+import android.app.Dialog;
+import android.app.TimePickerDialog;
 import android.os.Bundle;
 import android.text.format.DateFormat;
-import android.widget.TextView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TimePicker;
 
 public class EventCreation extends Activity {
 
-	private int start_year;
-	private int start_month;
-	private int start_day;
-	
+	private Calendar calendar_start;
+	private Calendar calendar_end;
+	private Calendar editing_calendar;
+
 	private java.text.DateFormat df;
-	
-	private TextView start_time;
-	private TextView end_time;
-	
+	private java.text.DateFormat tf;
+
+	private Button start_time;
+	private Button start_date;
+	private Button end_date;
+	private Button end_time;
+
+	static final int START_DATE_DIALOG = 0;
+	static final int START_TIME_DIALOG = 1;
+	static final int END_DATE_DIALOG = 2;
+	static final int END_TIME_DIALOG = 3;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.event_creation);
-		
+
 		df = DateFormat.getDateFormat(getApplicationContext());
-		start_time = (TextView)findViewById(R.id.editStartTime);
-		end_time = (TextView)findViewById(R.id.editEndTime);
+		tf = DateFormat.getTimeFormat(getApplicationContext());
+
+		start_date = set_up_button(R.id.editStartDate, START_DATE_DIALOG);
+		start_time = set_up_button(R.id.editStartTime, START_TIME_DIALOG);
+		end_date = set_up_button(R.id.editEndDate, END_DATE_DIALOG);
+		end_time = set_up_button(R.id.editEndTime, END_TIME_DIALOG);
 		
-		final Calendar c = GregorianCalendar.getInstance();
-		
-		start_year = c.get(Calendar.YEAR);
-		start_month = c.get(Calendar.MONTH);
-		start_day = c.get(Calendar.DAY_OF_MONTH);
-		
+		calendar_start = GregorianCalendar.getInstance();
+		calendar_end = GregorianCalendar.getInstance();
+		calendar_end.add(Calendar.HOUR, 1);
+
 		updateDates();
+	}
+	
+	private Button set_up_button(int id, final int dialog_id){
+		Button button = (Button) findViewById(id);
+		button.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				showDialog(dialog_id);
+			}
+		});
+		return button;
 	}
 
 	private void updateDates() {
-		Calendar d = GregorianCalendar.getInstance();
-		String text = df.format(d.getTime());
-		start_time.setText(text);
-		end_time.setText(text);
+		start_date.setText(df.format(calendar_start.getTime()));
+		start_time.setText(tf.format(calendar_start.getTime()));
+		end_date.setText(df.format(calendar_end.getTime()));
+		end_time.setText(tf.format(calendar_end.getTime()));
 	}
 
 	@Override
@@ -58,5 +83,46 @@ public class EventCreation extends Activity {
 		super.onResume();
 	}
 
-	
+	private TimePickerDialog.OnTimeSetListener mStartTimeSetListener = new TimePickerDialog.OnTimeSetListener() {
+		public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+			editing_calendar.set(Calendar.HOUR_OF_DAY, hourOfDay);
+			editing_calendar.set(Calendar.MINUTE, minute);
+			updateDates();
+		}
+	};
+
+	private OnDateSetListener mStartDateSetListener = new OnDateSetListener() {
+
+		public void onDateSet(DatePicker view, int year, int monthOfYear,
+				int dayOfMonth) {
+			editing_calendar.set(Calendar.YEAR, year);
+			editing_calendar.set(Calendar.MONTH, monthOfYear);
+			editing_calendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+			updateDates();
+		}
+	};
+
+	@Override
+	protected Dialog onCreateDialog(int id) {
+		if(id == START_TIME_DIALOG || id == START_DATE_DIALOG)
+			editing_calendar = calendar_start;
+		else
+			editing_calendar = calendar_end;
+		
+		if(id == START_TIME_DIALOG || id == END_TIME_DIALOG) {
+			return new TimePickerDialog(this, mStartTimeSetListener,
+					editing_calendar.get(Calendar.HOUR_OF_DAY),
+					editing_calendar.get(Calendar.MINUTE), false);
+		}
+		
+		else if(id == START_DATE_DIALOG || id == END_DATE_DIALOG) {
+			return new DatePickerDialog(this, mStartDateSetListener,
+					editing_calendar.get(Calendar.YEAR),
+					editing_calendar.get(Calendar.MONTH),
+					editing_calendar.get(Calendar.DAY_OF_MONTH));
+		}
+		else
+			return null;
+	}
+
 }
