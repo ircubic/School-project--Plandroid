21c200d6ff8f9bc5ebc4cdafc1c5d076cab80ca7
Daniel Bruce <ircubic@gmail.com>
2011-04-12 01:04:39 +0200

    Added skeleton for friends

---
+++ prototype/src/net/ircubic/eventmap/FriendInviting.java
@@ -0,0 +1,26 @@
+package net.ircubic.eventmap;
+
+import android.app.ListActivity;
+import android.os.Bundle;
+
+public class FriendInviting extends ListActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+	}
+
+	@Override
+	protected void onPause() {
+		// TODO Auto-generated method stub
+		super.onPause();
+	}
+
+	@Override
+	protected void onResume() {
+		// TODO Auto-generated method stub
+		super.onResume();
+	}
+
+}
